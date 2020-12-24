ce445722522121a494c2383cbdd9fc1e1ab2fed3
avbravo <avbravo@avbravo>
2020-12-24 16:07:26 -0500

    primera version

---
+++ src/main/java/com/avbravp/mytitlefx/App.java
@@ -0,0 +1,22 @@
+package com.avbravp.mytitlefx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import eu.hansolo.tilesfx.Demo;
+
+/**
+ * JavaFX App
+ */
+public class App {
+
+   
+    public static void main(String[] args) {
+//        launch();
+        Application.launch(Demo.class, args);
+
+    }
+
+}
